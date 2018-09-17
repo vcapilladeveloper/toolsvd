0e7e83d9983248e74765ae5e2f26df4e32806406
Victor Capilla <victor.capilla@studiogenesis.es>
2018-09-17 10:01:39 +0200

    first Commit

---
+++ ToolsVD/URLImageVD.swift
@@ -0,0 +1,38 @@
+//
+//  URLImageVD.swift
+//  ToolsVD
+//
+//  Created by Victor Capilla Studiogenesis on 17/9/18.
+//  Copyright © 2018 Victor Capilla Studiogenesis. All rights reserved.
+//
+
+import UIKit
+
+public class URLImageVD: UIImageView {
+    
+    public var placeholderImage : UIImage?
+    
+    public var url : NSURL? {
+        didSet {
+            self.image = placeholderImage
+            if let urlString = url?.absoluteString {
+                ImageLoaderVD.sharedLoader.imageForUrl(urlString: urlString) { [weak self] image, url in
+                    if let strongSelf = self {
+                        DispatchQueue.main.async(execute: { () -> Void in
+                            if strongSelf.url?.absoluteString == url {
+                                strongSelf.image = image ?? strongSelf.placeholderImage
+                            }
+                        })
+                    }
+                }
+            }
+        }
+    }
+    
+    public func setURL(url: NSURL?, placeholderImage: UIImage?) {
+        self.placeholderImage = placeholderImage
+        self.url = url
+    }
+    
+}
+
