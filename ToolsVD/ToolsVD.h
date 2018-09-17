8962a97fd829b5c95906dde8c7404c6e587273a4
Victor Capilla <victor.capilla@studiogenesis.es>
2018-09-17 09:39:10 +0200

    Initial Commit

---
+++ ToolsVD/ToolsVD.h
@@ -0,0 +1,19 @@
+//
+//  ToolsVD.h
+//  ToolsVD
+//
+//  Created by Victor Capilla Studiogenesis on 17/9/18.
+//  Copyright © 2018 Victor Capilla Studiogenesis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToolsVD.
+FOUNDATION_EXPORT double ToolsVDVersionNumber;
+
+//! Project version string for ToolsVD.
+FOUNDATION_EXPORT const unsigned char ToolsVDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToolsVD/PublicHeader.h>
+
+
