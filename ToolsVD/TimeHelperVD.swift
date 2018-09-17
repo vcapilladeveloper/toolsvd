a8c1fc64345980ff1ab69aeed9c9ceeebfb67b34
Victor Capilla Borrego <victor.capilla@gmail.com>
2018-09-17 23:53:44 +0200

    Change formula of TimeConverter

--- ToolsVD/TimeHelperVD.swift
+++ ToolsVD/TimeHelperVD.swift
@@ -11,6 +11,12 @@ import Foundation
 public class TimeHelperVD: NSObject {
     public func stringFromSecondsInFormat(_ seconds: Double) -> String {
         let secondsInt = Int(seconds)
-        return "\(secondsInt / 3600):\(secondsInt % 3600) / 60):\((secondsInt % 3600) % 60) time"
+        if secondsInt >= 3600 {
+            return "\(secondsInt/3600)h \((secondsInt % 3600)/60)m \((secondsInt % 3600) % 60)s"
+        } else if secondsInt >= 60 {
+            return "\(secondsInt/60)m \(secondsInt % 60)s"
+        } else {
+            return "\(secondsInt)s"
+        }
     }
 }
